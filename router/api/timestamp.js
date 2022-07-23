91161f9d9d5e0e52287d15465e001257ccbbf63b
Amir Hosein Nazari <nazari.amirhosein79@gmail.com>
2022-07-23 15:04:31 +0400

    cloned from fcc

--- router/api/timestamp.js
+++ router/api/timestamp.js
@@ -9,23 +9,25 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:date', (req, res) => {
-  const str = req.params.date;
+  let date_string = req.params.date;
 
   const unixRegex = /\b\d{13}\b/;
   const dateRegex = /\b\d{4}-\d{2}\-\d{2}\b/;
 
-  const unixIsValid = String(str).match(unixRegex);
-  const dateIsValid = String(str).match(dateRegex);
+  const unixIsValid = String(date_string).match(unixRegex);
+  const dateIsValid = String(date_string).match(dateRegex);
 
   if (unixIsValid) {
+    date_string = Number(date_string);
     res.json({
-      unix: new Date(Number(req.params.date)).getTime(),
-      utc: new Date(Number(req.params.date)).toUTCString(),
+      unix: new Date(date_string).getTime(),
+      utc: new Date(date_string).toUTCString(),
     });
   } else if (dateIsValid) {
+    date_string = String(date_string);
     res.json({
-      unix: new Date(String(req.params.date)).getTime(),
-      utc: new Date(String(req.params.date)).toUTCString(),
+      unix: new Date(date_string).getTime(),
+      utc: new Date(date_string).toUTCString(),
     });
   } else {
     res.json({
